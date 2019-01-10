a8d9bc3c02af6b5a54f21ffb0b97589178ef6999
Pavle Papuga <Pavle.Papuga@netcetera.com>
2019-01-10 08:48:18 +0100

    Wsdl used instead of xsd

--- src/main/java/hello/WebServiceConfig.java
+++ src/main/java/hello/WebServiceConfig.java
@@ -8,9 +8,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
-import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
-import org.springframework.xml.xsd.SimpleXsdSchema;
-import org.springframework.xml.xsd.XsdSchema;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 
 @EnableWs
 @Configuration
@@ -24,17 +22,8 @@ public class WebServiceConfig extends WsConfigurerAdapter {
   }
 
   @Bean(name = "countries")
-  public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
-    DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-    wsdl11Definition.setPortTypeName("CountriesPort");
-    wsdl11Definition.setLocationUri("/ws");
-    wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
-    wsdl11Definition.setSchema(countriesSchema);
-    return wsdl11Definition;
-  }
-
-  @Bean
-  public XsdSchema countriesSchema() {
-    return new SimpleXsdSchema(new ClassPathResource("countries.xsd"));
+  public SimpleWsdl11Definition defaultWsdl11Definition() {
+    ClassPathResource classPathResource = new ClassPathResource("countries.wsdl");
+    return new SimpleWsdl11Definition(classPathResource);
   }
 }
